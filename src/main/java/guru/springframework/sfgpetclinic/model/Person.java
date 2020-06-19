6fd080b0e2eaeb1dc85a9bcec4416f63176eab4d
almensour <almensour@yahoo.fr>
2020-06-19 18:53:57 +0100

    Adding POJO. Closed #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,23 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+
+    private String firstname;
+    private String lastename;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastename() {
+        return lastename;
+    }
+
+    public void setLastename(String lastename) {
+        this.lastename = lastename;
+    }
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vep.java
@@ -0,0 +1,5 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Vep extends Person {
+
+}
